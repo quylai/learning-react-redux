8d06cc13055e445a297cb9e72b1e7515bd12c5e0
quyl <quylai@gmail.com>
2019-09-15 15:37:35 -0500

    got table to generated

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,10 @@
 import React from "react";
 import Form from "./components/Form";
-// import Get from "./components/Get";
-import Post from "./components/Post";
-
 
 function App() {
   return( 
     <>
       <Form />
-      {/* <Get /> */}
-      <Post />
     </>
   );
 }
--- server.js
+++ server.js
@@ -8,12 +8,21 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
+// Requiring our models for syncing
+var db = require("./client/models");  //q
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));  //q
+app.use(express.json());  //q
+
 // Send every request to the React app
 // Define any API routes before this runs
 app.get("*", function(req, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
-app.listen(PORT, function() {
-  console.log(`🌎 ==> API server now on port ${PORT}!`);
+db.sequelize.sync().then(function() {  //q
+  app.listen(PORT, function() {
+    console.log(`🌎 ==> API server now on port ${PORT}!`);
+  });
 });
