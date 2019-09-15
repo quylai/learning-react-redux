86fc8240b98a6ac1daf781073c947f127a439dd4
quyl <quylai@gmail.com>
2019-09-15 05:30:02 -0500

    generated folders config and models with cmds, sequelize init:config and sequelize init:models

---
+++ schema.sql
@@ -0,0 +1,7 @@
+
+
+-- need to manually process these code via MySQL Workbench or Sequel Pro --
+DROP DATABASE IF EXISTS `valet_db`;
+CREATE DATABASE `valet_db`;
+
+/*-------------------------------------------------------------------------------*/
