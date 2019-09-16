ef13e83b689f3e347f0b3fd323aadefc074845d4
quyl <quylai@gmail.com>
2019-09-16 03:27:06 -0500

    14 - binding event handlers; done

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,10 @@ import React from "react";
 // import Counter from "./components/Counter.js";
 // import Greet from "./components/Greet.js";
 // import Welcome from "./components/Welcome.js";
-import FunctionClick from "./components/FunctionClick.js";
-import ClassClick from "./components/ClassClick.js";
+// import FunctionClick from "./components/FunctionClick.js";
+// import ClassClick from "./components/ClassClick.js";
+import EventBind from "./components/EventBind.js";
+
 
 
 
@@ -13,8 +15,10 @@ function App() {
     <div className="App">
       {/* <Greet name="Diana" heroName="Wonder Woman" /> */}
       {/* <Welcome name="Bruce" heroName="Batman" /> */}
-      <FunctionClick />
-      <ClassClick />
+      {/* <FunctionClick /> */}
+      {/* <ClassClick /> */}
+      <EventBind />
+
 
     </div>
     </>
