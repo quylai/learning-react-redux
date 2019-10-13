513bace3f9952e449527a8d1b03b585d28f7d5d0
quyl <quylai@gmail.com>
2019-10-12 20:43:20 -0500

    06 done

--- react-redux-stuff/REDUX-DEMO/index.js
+++ react-redux-stuff/REDUX-DEMO/index.js
@@ -20,6 +20,7 @@ const initialState = {
 const reducer = (state = initialState, action) => {
   switch(action.type) {
     case BUY_CAKE: return {
+      ...state,
       numOfCakes: state.numOfCakes - 1
     }
 
