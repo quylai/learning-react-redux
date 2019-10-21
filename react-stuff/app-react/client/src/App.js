b4461f7689b401a1bb628fc7ca969b4e231567cc
quyl <quylai@gmail.com>
2019-10-21 02:36:21 -0500

    need to understand how child access parent data

--- react-stuff/app-react/client/src/App.js
+++ react-stuff/app-react/client/src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
 import "./App.css";
-import TestComp from "./components/TestComp";
+import FocusInput from "./components/FocusInput";
 
 class App extends Component {
+  
   render() {
     return(
       <div className="App">
-        <TestComp />
+        <FocusInput />
       </div>
     );
   }
